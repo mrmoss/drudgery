ef1fb40495dbf48ae7ca831dc18439df1265ec69
Mike Moss <mrmoss@alaska.edu>
2013-12-18 15:03:58 -0900

    Made archive/delete button work.

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,70 @@
 #include <iostream>
 #include <string>
 
+class task_list_ui
+{
+	public:
+		task_list_ui(const double x=0,const double y=0):x(x),y(y),display_width(0),display_height(0),visible(true)
+		{
+			list_ui.width=480;
+		}
+
+		void loop(const double dt)
+		{
+			list.loop();
+
+			if(visible)
+			{
+				list_ui.y=y-list_ui.display_height/2.0;
+				display_width=list_ui.display_width;
+				display_height=list_ui.display_height;
+
+				std::vector<std::string> temp;
+
+				for(unsigned int ii=0;ii<list.size();++ii)
+					temp.push_back(list[ii].name);
+
+				list_ui.options=temp;
+				list_ui.loop(dt);
+
+				if(list_ui.value>=list.size())
+					list_ui.value=-1;
+			}
+		}
+
+		void draw()
+		{
+			if(visible)
+				list_ui.draw();
+		}
+
+		double x;
+		double y;
+		double display_width;
+		double display_height;
+		bool visible;
+
+		task_list list;
+		msl::list list_ui;
+};
+
 class menu_bar
 {
 	public:
-		menu_bar(const double x=0,const double y=0):x(x),y(y),display_width(0),display_height(0),escape("Exit"),add_task("Add")
+		menu_bar(const double x=0,const double y=0):x(x),y(y),display_width(0),display_height(0),
+			escape("Exit"),add_task("Add Task")
 		{
-			escape.padding=add_task.padding=10;
+			escape.padding=add_task.padding=archive_task.padding=10;
 
 			h0.widgets.push_back(&escape);
 			h0.widgets.push_back(&add_task);
-			h0.background_color_to.a=h0.outline_color.a=0;
+			h0.widgets.push_back(&archive_task);
+			h0.background_color_to.a=h0.background_color_from.a=h0.outline_color.a=0;
 
 			v0.widgets.push_back(&h0);
 		}
 
-		void loop(const double dt)
+		void loop(const double dt,task_list_ui& active,task_list_ui& archive)
 		{
 			v0.y=y-v0.display_height/2.0;
 			v0.loop(dt);
@@ -29,6 +78,48 @@ class menu_bar
 
 			if(escape.pressed)
 				exit(0);
+
+			if(active.visible)
+			{
+				if((int)active.list_ui.value>=0)
+					archive_task.disabled=false;
+				else
+					archive_task.disabled=true;
+
+				add_task.disabled=false;
+				archive_task.value="Archive Task";
+
+				if(add_task.pressed&&active.visible)
+				{
+					task new_task(date(18,12,2013),"","New Task",0,0);
+
+					if((int)active.list_ui.value>=0)
+						active.list.insert(new_task,active.list_ui.value);
+					else
+						active.list.add(new_task);
+				}
+
+				if(archive_task.pressed&&(int)active.list_ui.value>=0)
+				{
+					if(active.list[active.list_ui.value].time_working>0)
+						archive.list.add(active.list[active.list_ui.value]);
+
+					active.list.remove(active.list_ui.value);
+				}
+			}
+			else if(archive.visible)
+			{
+				if((int)archive.list_ui.value>=0)
+					archive_task.disabled=false;
+				else
+					archive_task.disabled=true;
+
+				add_task.disabled=true;
+				archive_task.value="Delete Task";
+
+				if(archive_task.pressed&&(int)archive.list_ui.value>=0)
+					archive.list.remove(archive.list_ui.value);
+			}
 		}
 
 		void draw()
@@ -43,54 +134,14 @@ class menu_bar
 
 		msl::button escape;
 		msl::button add_task;
+		msl::button archive_task;
 		msl::hdock h0;
 		msl::vdock v0;
 };
 
-class task_list_ui
-{
-	public:
-		task_list_ui(const double x=0,const double y=0):x(x),y(y),display_width(0),display_height(0)
-		{
-			list_ui.width=480;
-		}
-
-		void loop(const double dt)
-		{
-			list.loop();
-
-			list_ui.y=y-list_ui.display_height/2.0;
-			display_width=list_ui.display_width;
-			display_height=list_ui.display_height;
-
-			std::vector<std::string> temp;
-
-			for(unsigned int ii=0;ii<list.size();++ii)
-				temp.push_back(list[ii].name);
-
-			list_ui.options=temp;
-			list_ui.loop(dt);
-
-			if(list_ui.value>=list.size())
-				list_ui.value=-1;
-		}
-
-		void draw()
-		{
-			list_ui.draw();
-		}
-
-		double x;
-		double y;
-		double display_width;
-		double display_height;
-
-		task_list list;
-		msl::list list_ui;
-};
-
 menu_bar menu(0,0);
-task_list_ui tasks(0,0);
+task_list_ui active(0,0);
+task_list_ui archive(0,0);
 
 task_ui task_viewer(0,0);
 
@@ -105,44 +156,74 @@ void setup()
 	msl::set_text_font("Ubuntu-B.ttf");
 	msl::set_text_size(12);
 
-	tasks.list.add(task(date(18,12,2013),"I need to add tasks to Drudgery.","My First Task",0,0));
+	active.list.add(task(date(18,12,2013),"I need to add tasks to Drudgery.","My First Task",0,0));
+	archive.visible=false;
+	active.visible=!archive.visible;
 }
 
 void loop(const double dt)
 {
 	menu.y=msl::window_height/2.0;
-	tasks.y=msl::window_height/2.0-menu.display_height;
-	task_viewer.y=msl::window_height/2.0-menu.display_height-tasks.display_height-task_viewer.display_height/2.0;
+	active.y=msl::window_height/2.0-menu.display_height;
+	archive.y=msl::window_height/2.0-menu.display_height;
 
-	menu.loop(dt);
+	if(msl::input_check_pressed(kb_f1))
+	{
+		archive.visible=!archive.visible;
+		active.visible=!archive.visible;
+	}
 
-	if(menu.add_task.pressed)
-		tasks.list.add(task(date(18,12,2013),"","New Task",0,0));
+	if(active.visible)
+	{
+		task_viewer.working_on.disabled=false;
+		task_viewer.modify.disabled=false;
 
-	if(msl::input_check_pressed(kb_f1)&& tasks.list.size()>0)
-		tasks.list.remove(tasks.list.size()-1);
+		task_viewer.y=msl::window_height/2.0-menu.display_height-active.display_height-task_viewer.display_height/2.0;
+	}
+	else if(archive.visible)
+	{
+		task_viewer.working_on.value=false;
+		task_viewer.modify.value=false;
+		task_viewer.working_on.disabled=true;
+		task_viewer.modify.disabled=true;
 
-	tasks.loop(dt);
+		task_viewer.y=msl::window_height/2.0-menu.display_height-archive.display_height-task_viewer.display_height/2.0;
+	}
 
-	tasks.list_ui.disabled=task_viewer.modify.value;
+	menu.loop(dt,active,archive);
 
-	if((int)tasks.list_ui.value>=0)
-		task_viewer.loop(dt,tasks.list[tasks.list_ui.value]);
+	active.loop(dt);
+	archive.loop(dt);
+
+	active.list_ui.disabled=task_viewer.modify.value;
+
+	if(active.visible&&(int)active.list_ui.value>=0)
+		task_viewer.loop(dt,active.list[active.list_ui.value]);
+
+	if(archive.visible&&(int)archive.list_ui.value>=0)
+		task_viewer.loop(dt,archive.list[archive.list_ui.value]);
 }
 
 void draw()
 {
 	menu.draw();
 
-	unsigned int old_index=tasks.list_ui.value;
-	tasks.draw();
-	unsigned int new_index=tasks.list_ui.value;
+	unsigned int old_index=active.list_ui.value;
+	active.draw();
+	unsigned int new_index=active.list_ui.value;
 
-	if((int)tasks.list_ui.value>=0)
+	archive.draw();
+
+	if(active.visible&&(int)active.list_ui.value>=0)
 	{
 		if(old_index!=new_index&&(int)old_index>=0)
-			task_viewer.working_on.value=tasks.list[tasks.list_ui.value].working_on;
+			task_viewer.working_on.value=active.list[active.list_ui.value].working_on;
 
+		task_viewer.draw();
+	}
+
+	if(archive.visible&&(int)archive.list_ui.value>=0)
+	{
 		task_viewer.draw();
 	}
 }
